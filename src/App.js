297f97a4bc829c57df2b6a8e9850e8de07d52b18
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-21 10:33:36 +0900

    styled-components adapting and extending
    props 속성 부여, 컴포넌트에 다른 컴포넌트 속성 불러오기

--- src/App.js
+++ src/App.js
@@ -4,31 +4,29 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: orange;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="orange" />
     </Father>
   );
 }
 
 export default App;
+
+// 똑같은 styled-component를 사용해도 props가 다르면 react에서 알아서 각 컴포넌트마다 고유한 className 부여
+
+// styled.div`` => 기본 사용방법
+// props를 사용해서 각 컴포넌트마다 다르게 부여할 속성 지정가능 => background-color : ${(props) => props.bgColor}
+// styled(Box)`` => Box의 속성을 다 들고 와줌.
