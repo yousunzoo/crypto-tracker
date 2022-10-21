ee41c3b4a392856edfc7f7a56bd84130db9fa04a
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-21 11:11:19 +0900

    animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const rotationAnimation = keyframes`
+0% {
+  transform :rotate(0deg);
+  border-radius:0px;
+}
+50% {
+  transform : rotate(360deg);
+  border-radius : 100px;
+}
+100% {
+  transform :rotate(0deg);
+  border-radius:0px;
+}
 `;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="orange" />
-    </Father>
+    <Wrapper>
+      <Box />
+    </Wrapper>
   );
 }
 
@@ -30,3 +40,7 @@ export default App;
 // styled.div`` => 기본 사용방법
 // props를 사용해서 각 컴포넌트마다 다르게 부여할 속성 지정가능 => background-color : ${(props) => props.bgColor}
 // styled(Box)`` => Box의 속성을 다 들고 와줌.
+
+// 컴포넌트의 태그를 바꾸고 싶은데 스타일은 바꾸고 싶지 않을 때 => 컴포넌트 안에 `as="header"` 작성
+
+// animation
