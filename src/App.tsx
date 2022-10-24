ce618334f5e04fa2ee14d53cc4daeaea4cf3ac70
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-10-24 09:37:37 +0900

    typescript theme 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
-import React, { useState } from "react";
-
+import React from "react";
+import styled from "styled-components";
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello</H1>
+    </Container>
   );
 }
 
